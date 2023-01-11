5c39b777de3646060cf7df8eeaf0eb878a49f83d
Reda GHARBI <reda6gharbigmail.com>
2023-01-11 09:23:50 +0100

    main recoding

--- datasets.py
+++ datasets.py
@@ -6,14 +6,33 @@ from collections import Counter
 import torch
 import wget
 from matplotlib import pyplot as plt
+
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+
+import src as models
 
 data_sets_urls = {
     'cifar10':
         'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
     'cifar100':
         'http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz',
+}
+
+DATASETS = {
+    'cifar10': {
+        'num_classes': 10,
+        'img_size': 32,
+        'mean': [0.4914, 0.4822, 0.4465],
+        'std': [0.2470, 0.2435, 0.2616]
+    },
+    'cifar100': {
+        'num_classes': 100,
+        'img_size': 32,
+        'mean': [0.5071, 0.4867, 0.4408],
+        'std': [0.2675, 0.2565, 0.2761]
+    }
 }
 
 data_sets_path = 'datasets/'
@@ -69,12 +88,35 @@ def dataset_exploration(dataset):
 
 if __name__ == "__main__":
     data_download()
-    train_dataset = datasets.CIFAR100(
-        root="datasets",
+
+    # dataset getting
+    args_dataset = "cifar100"
+
+    img_size = DATASETS[args_dataset]['img_size']
+    num_classes = DATASETS[args_dataset]['num_classes']
+    img_mean, img_std = DATASETS[args_dataset]['mean'], DATASETS[args_dataset]['std']
+
+    train_dataset = datasets.__dict__[args_dataset.upper()](
+        root=data_sets_path,
         train=True,
         transform=ToTensor(),
         download=True
     )
 
+    val_dataset = datasets.__dict__[args_dataset.upper()](
+        root=data_sets_path,
+        train=False,
+        download=False,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(img_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=img_mean, std=img_std),
+            ]
+        )
+    )
 
+    # model getting
+    args_model = 'cct_7_3x1_32_c100'
 
+    model = models.__dict__[args_model]()
